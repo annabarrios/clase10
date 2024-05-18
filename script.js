539c447c235016cd0d3d17361fe6b50b1d2c5694
annabarrios <166767059+annabarrios@users.noreply.github.com>
2024-05-17 21:42:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var nombre="Analia"//string
+var apellido="Barrios"//string
+
+console.log(nombre+" "+apellido)
+
+
+
+var numero_1=30;
+var numero_2=25;
+
+var suma=numero_1+numero_2;
+
+console.log("el resultado de la suma es: "+suma);
+
+var numero_3=15;
+
+var suma_1=(suma)+numero_3;
+
+console.log("el resultado es: "+suma_1);
